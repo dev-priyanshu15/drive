c5605765c7ff2f1f66d484e7f9eef876a915b462
Priyanshu Singh <s.priyanshu.coder@gmail.com>
2026-02-13 15:04:45 +0530

    ab db or node connect ho gya to scema banaye wo dekh lena db.js me phir usko export karne k liye const user=mongoose.model('user',userschema); module.exports kiye usko userroute me import kar diye

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body,validationResult } = require('express-validator');
+const userModel = require('../models/user.model');
 // router.get('/test', (req, res) => {
 //     res.send('user test route');
 // });
@@ -14,13 +15,15 @@ router.post(
   body("password").trim().isLength({ min: 5 }),
   body("username").trim().isLength({ min: 3 }),
 
-  (req, res) => {
+  async (req, res) => {
 
     const errors = validationResult(req);
     // Handle registration logic here
    if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array(),message: 'invalid data' });
     }
-    res.send(errors);
+    const { username, email, password } = req.body;
+    const newUser = await userModel.create({ username, email, password });
+    res.json(newUser);
 });
 module.exports = router;
