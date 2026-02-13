b08341ec8e9743d3d8bccaa626a640eaa6a7a59a
Priyanshu Singh <s.priyanshu.coder@gmail.com>
2026-02-13 13:20:20 +0530

    added view engine or agar ejs ka use kiye to view folder me index.ejs banana padega phir add.js pe isko render kiye / route pe kyunki ejs wala ko render karte hai

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 app.listen(3000, () => {
   console.log('Server is  running  on port 3000!');
